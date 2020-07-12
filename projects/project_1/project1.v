814af3b98505ae71a3ff760099907de2d377c3a6
nnikolov3 <nnikolov@pdx.edu>
2020-07-12 14:25:52 -0700

    project1

---
+++ projects/project_1/project1.v
@@ -0,0 +1,31 @@
+// ************************************************
+// Project 1
+// ECE582
+// Nikolay Nikolov
+// Summer 2020
+// Task 1
+// ************************************************
+
+module project1_task_1_1 (output wire a,
+		 input wire x1, x2, y1, y2);
+
+   // Internal Nets
+   wire 		    m0, m1, m2, m3;
+   wire 		    a0, a1;
+   // AND
+   assign m0 = x1 & y1;
+   assign m2 = x2 & y2;
+   // NOR
+   assign m1 = x1 ~| y1;
+   assign m3 = x2 ~| y2;
+   // OR
+   assign a0 = m0 | m1;
+   assign a1 = m2 | m3;
+   // Out
+   assign a = a1 | a0;
+
+endmodule // project1_task_1_1
+
+   
+   
+  
---
+++ projects/project_1/project1_task1_2.v
@@ -0,0 +1,22 @@
+// ************************************************
+// Project 1
+// ECE582
+// Nikolay Nikolov
+// Summer 2020
+// Task 1_C2
+// ************************************************
+
+module project1_task1_2 (output wire b,
+			 input wire x1, x2, y1, y2);
+
+   // Internal Nets
+   wire 			    m0, m1, m2, m3;
+   // OR
+   assign m0 = x1 | y1 | x2 | y2;
+   assign m1 = ~x1 ~| ~y1 | x2 | y2;
+   assign m2 = x1 | ~y1 | ~x2 | y2;
+   assign m3 = ~x1 | ~y1 | ~x2 | y2;
+   // Out
+   assign b = m0 | m1 | m2 | m3;
+
+endmodule // project1_task1_2
